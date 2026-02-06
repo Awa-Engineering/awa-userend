f0b96d1246763e0f457d6bfb774eabe59cbfae1d
codystack <ebuzzadvert@gmail.com>
2026-02-06 13:35:36 +0100

    About Page Added

---
+++ ceo.php
@@ -0,0 +1,49 @@
+<?php
+include "./components/header.php";
+include "./components/navbar-alt.php";
+?>
+
+    <div class="breadcumb-wrapper" data-bg-src="./assets/img/faq-breacrumb.jpeg">
+        <div class="breadcumb-overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcumb-content">
+                        <h1 class="breadcumb-title">The CEO's Desk</h1>
+                        <ul class="breadcumb-menu">
+                            <li><a href="./">Home</a></li>
+                            <li>CEO Desk</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="space">
+        <div class="container">
+            <div class="team-details-about-info">
+                <div class="row gy-40 gx-80 align-items-center">
+                    <div class="col-lg-6 position-relative">
+                        <div class="team-details-thumb">
+                            <img src="./assets/img/ceo.png" alt="team image">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="about-page-box">
+                            <h2 class="mt-n3 fw-semibold mb-1">Engr. Victoria Wobo Nyeche PhD</h2>
+                        </div>
+                        <p class="mt-30">Engr. Victoria Nyeche is the Chief Executive Officer has over two decades of professional experience as an Environmentalist in the private and public sector.</p>
+                        <p>She holds a Doctorate degree in Environmental Resources Management as well as a Masters degree in Chemical Engineering. She is a COREN registered Chemical Engineer, a registered Environmental Professional, a fellow of the Nigerian Environmental Society and a member of Nigerian Society of Engineers.</p>
+                        <p>Prior to the establishment of Awa Engineering Co. Ltd, Engr. Nyeche worked with Schlumberger Geoquest Ltd, as well as held several leadership positions in the public service where she has acquired many years of relevant experience in occupational health and safety and environmental management. She has published several scholarly works on Climate Change, attended several professional International conferences on Leadership and Environment.</p>
+                        <div class="th-social style7 mt-35">
+                            <a target="_blank" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                            <a target="_blank" href="https://linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php include "./components/footer.php"; ?>
