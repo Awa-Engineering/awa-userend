677f74c360a7cdba3faa52ccc7730cfcc3c8ce94
codystack <ebuzzadvert@gmail.com>
2026-02-07 09:59:52 +0100

    Index modified

--- index.php
+++ index.php
@@ -15,9 +15,14 @@ include "./components/navbar.php";
                         <div class="hero-inner">
                             <div class="row justify-content-center">
                                 <div class="col-xl-10">
-                                    <div class="hero-style10 text-center"><span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Your Vision, Our Expertise</span>
-                                        <h1 class="hero-title text-white"><span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Quality Construction, Timeless Designs</span></h1>
-                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s"><a href="contact.html" class="th-btn style4 th-btn-icon">Discover More</a></div>
+                                    <div class="hero-style10 text-center">
+                                        <span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Expertise & Innovation</span>
+                                        <h1 class="hero-title text-white">
+                                            <span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Engineering Solutions, Driving Progress</span>
+                                        </h1>
+                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s">
+                                            <a href="services" class="th-btn style4 th-btn-icon">Learn More</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -26,14 +31,21 @@ include "./components/navbar.php";
                 </div>
 
                 <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_10_2.jpg" data-mask-src="assets/img/hero/hero_10_bg_mask.png"><img src="assets/img/hero/hero_bg_overlay10_1.png" alt="img"></div>
+                    <div class="th-hero-bg" data-bg-src="./assets/img/project/project3-1.jpg" data-mask-src="./assets/img/hero-bg-mask.png">
+                        <img src="./assets/img/hero-overlay.png" alt="img">
+                    </div>
                     <div class="container">
                         <div class="hero-inner">
                             <div class="row justify-content-center">
                                 <div class="col-xl-10">
-                                    <div class="hero-style10 text-center"><span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Your Vision, Our Expertise</span>
-                                        <h1 class="hero-title text-white"><span class="title1" data-ani="slideinup" data-ani-delay="0.4s">We Construction Your Modern Home</span></h1>
-                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s"><a href="contact.html" class="th-btn style4 th-btn-icon">Discover More</a></div>
+                                    <div class="hero-style10 text-center">
+                                        <span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Quality & Reliability</span>
+                                        <h1 class="hero-title text-white">
+                                            <span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Excellence in Every Project</span>
+                                        </h1>
+                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s">
+                                            <a href="services" class="th-btn style4 th-btn-icon">Our Services</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -42,62 +54,21 @@ include "./components/navbar.php";
                 </div>
 
                 <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_10_3.jpg" data-mask-src="assets/img/hero/hero_10_bg_mask.png"><img src="assets/img/hero/hero_bg_overlay10_1.png" alt="img"></div>
-                    <div class="container">
-                        <div class="hero-inner">
-                            <div class="row justify-content-center">
-                                <div class="col-xl-10">
-                                    <div class="hero-style10 text-center"><span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Your Vision, Our Expertise</span>
-                                        <h1 class="hero-title text-white"><span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Building Dreams Creating Reality</span></h1>
-                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s"><a href="contact.html" class="th-btn style4 th-btn-icon">Discover More</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="th-hero-bg" data-bg-src="./assets/img/project/project7-1.jpg" data-mask-src="./assets/img/hero-bg-mask.png">
+                        <img src="./assets/img/hero-overlay.png" alt="img">
                     </div>
-                </div>
-
-                <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_10_1.jpg" data-mask-src="assets/img/hero/hero_10_bg_mask.png"><img src="assets/img/hero/hero_bg_overlay10_1.png" alt="img"></div>
                     <div class="container">
                         <div class="hero-inner">
                             <div class="row justify-content-center">
                                 <div class="col-xl-10">
-                                    <div class="hero-style10 text-center"><span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Your Vision, Our Expertise</span>
-                                        <h1 class="hero-title text-white"><span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Quality Construction, Timeless Designs</span></h1>
-                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s"><a href="contact.html" class="th-btn style4 th-btn-icon">Discover More</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_10_2.jpg" data-mask-src="assets/img/hero/hero_10_bg_mask.png"><img src="assets/img/hero/hero_bg_overlay10_1.png" alt="img"></div>
-                    <div class="container">
-                        <div class="hero-inner">
-                            <div class="row justify-content-center">
-                                <div class="col-xl-10">
-                                    <div class="hero-style10 text-center"><span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Your Vision, Our Expertise</span>
-                                        <h1 class="hero-title text-white"><span class="title1" data-ani="slideinup" data-ani-delay="0.4s">We Construction Your Modern Home</span></h1>
-                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s"><a href="contact.html" class="th-btn style4 th-btn-icon">Discover More</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_10_3.jpg" data-mask-src="assets/img/hero/hero_10_bg_mask.png"><img src="assets/img/hero/hero_bg_overlay10_1.png" alt="img"></div>
-                    <div class="container">
-                        <div class="hero-inner">
-                            <div class="row justify-content-center">
-                                <div class="col-xl-10">
-                                    <div class="hero-style10 text-center"><span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Your Vision, Our Expertise</span>
-                                        <h1 class="hero-title text-white"><span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Building Dreams Creating Reality</span></h1>
-                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s"><a href="contact.html" class="th-btn style4 th-btn-icon">Discover More</a></div>
+                                    <div class="hero-style10 text-center">
+                                        <span class="hero-sub-title text-theme" data-ani="slideinup" data-ani-delay="0.3s">Sustainable Impact</span>
+                                        <h1 class="hero-title text-white">
+                                            <span class="title1" data-ani="slideinup" data-ani-delay="0.4s">Building a Sustainable Future</span>
+                                        </h1>
+                                        <div class="btn-wrap justify-content-center" data-ani="slideinup" data-ani-delay="0.7s">
+                                            <a href="request-quote" class="th-btn style4 th-btn-icon">Get a Quote</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -106,13 +77,82 @@ include "./components/navbar.php";
                 </div>
             </div>
         </div>
+        
         <div class="hero-bottom-wrap">
             <div class="about-tag">
-                <div class="about-experience-tag"><span class="circle-title-anime">Explore and find Your Dream place</span></div>
+                <div class="about-experience-tag"><span class="circle-title-anime">Explore. Innovate. Build with AWA</span></div>
                 <div class="about-tag-thumb scroll-down"><a href="#about-sec" class="hero-scroll-wrap"></a></div>
             </div>
         </div>
     </div>
+
+    <!-- counter section -->
+    <div class="bg-smoke2 overflow-hidden pt-60 pt-xl-0 pb-60">
+        <div class="container">
+            <div class="counter-card-wrap">
+                <div class="counter-card style4">
+                    <div class="media-body">
+                        <h2 class="box-number"><span class="counter-number">100</span>+</h2>
+                        <p class="box-text">Completed Projects</p>
+                    </div>
+                </div>
+                <div class="counter-card style4">
+                    <div class="media-body">
+                        <h2 class="box-number"><span class="counter-number">350</span>+</h2>
+                        <p class="box-text">Total Workforce</p>
+                    </div>
+                </div>
+                <div class="counter-card style4">
+                    <div class="media-body">
+                        <h2 class="box-number"><span class="counter-number">100</span>+</h2>
+                        <p class="box-text">Satisfied Clients</p>
+                    </div>
+                </div>
+                <div class="counter-card style4">
+                    <div class="media-body">
+                        <h2 class="box-number"><span class="counter-number">10</span>+</h2>
+                        <p class="box-text">Years of Experience</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- about section -->
+    <div class="about-area-10 z-index-common position-relative space overflow-hidden" id="about-sec">
+        <div class="container">
+            <div class="row gx-80 justify-content-between align-items-center">
+                <div class="col-xl-6 mb-40 mb-xl-0">
+                    <div class="img-box10">
+                        <div class="img1"><img src="./assets/img/about_img.jpg" alt="img"></div>
+                        <div class="img2"><img src="./assets/img/about_img1.jpg" alt="img"></div>
+                        <div class="img3"><img src="./assets/img/about_img2.jpg" alt="img"></div>
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                    <div class="title-area mb-32"><span class="sub-title">About AWA Engineering</span>
+                        <h2 class="sec-title">Expertise. Innovation. Reliability.</h2>
+                        <p class="sec-text">AWA Engineering Company Limited delivers innovative engineering and environmental solutions across the oil & gas and public sectors. With a team of experienced professionals, cutting-edge technology, and strategic partnerships, we provide high-quality, efficient, and sustainable project execution that exceeds expectations.</p>
+                    </div>
+                    <div class="checklist style5">
+                        <ul>
+                            <li>Comprehensive project planning and feasibility studies</li>
+                            <li>Guaranteed quality and client satisfaction</li>
+                            <li>Engineering solutions tailored to your project needs</li>
+                            <li>Sustainable and environmentally responsible practices</li>
+                        </ul>
+                    </div>
+                    <div class="btn-wrap">
+                        <div class="btn-wrap mt-35 justify-content-lg-start justify-content-center">
+                            <a href="about" class="th-btn style3 th-btn-icon">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include "./components/clients.php"; ?>
 
 
 <?php include "./components/footer.php"; ?>
